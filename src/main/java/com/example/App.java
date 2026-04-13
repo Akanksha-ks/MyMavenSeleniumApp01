ae01ca8d4ce5ec0855a947c0cb7bd74f93f1386f
Akanksha KS <ksakanksha022@gmail.com>
2026-04-13 08:39:51 +0530

    Refactor App class and add headless option

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,22 +5,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class App
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) {
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--headless=new");   // 
 
         WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
 
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+
+        driver.quit();
     }
 }
